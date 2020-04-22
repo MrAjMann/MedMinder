bb741f75871e39e3013d303601ed0c0d370ab1d8
MrAjMann <anthonyjmann87@gmail.com>
2020-04-22 10:24:57 +1000

    Created a main application file
    Added the main.rb file with some commented out application information

---
+++ src/main.rb
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------------------------------------------------------
+#                                 💊 MedMinder
+#                                  Terminal App
+#
+#                             Designed and Developed by
+#                                 Anthony Mann 
+#                 For my Coder Academy: Ruby Terminal Assessment
+#                   
+#  Version 1.0
+#  21 April 2020
+#---------------------------------------------------------------------------------------------------------------------
+
+puts "Welcome to MedMinder"
